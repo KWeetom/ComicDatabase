28c180775aaf12c6a18ea1d8f396a0c94e76ed64
Kadeem Weetom <kadeemweetom@gmail.com>
2018-01-16 18:41:57 -0500

    fixed node.js and made it run a server. Added a but of html

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
-// var api = require('marvel-api');
+console.log('Started');
+var http = require('http');
+var fs = require('fs');
 
-// var marvel = api.createClient({
-// publicKey: 'b418924bd54847ace74ceaa0e9b44017',
-// privateKey: '16daedc6fc0a02f239a9ad700e10b526498a9afd'
+var api = require('marvel-api');
 
-// });
+var marvel = api.createClient({
+publicKey: 'b418924bd54847ace74ceaa0e9b44017',
+privateKey: '16daedc6fc0a02f239a9ad700e10b526498a9afd'
 
+});
 
-
-api('characters', {
-  publicKey: 'b418924bd54847ace74ceaa0e9b44017',
-  privateKey: '.. your private key ..',
-  timeout: 4000,
-  query: {
-    limit: 50
-  }
-}, function (err, body) {
-  if (err) throw err
+// api('characters', {
+//   publicKey: 'b418924bd54847ace74ceaa0e9b44017',
+//   privateKey: '.. your private key ..',
+//   timeout: 4000,
+//   query: {
+//     limit: 50
+//   }
+// }, function (err, body) {
+//   if (err) throw err
   
-  // total # of items 
-  console.log(body.data.total)
+//   // total # of items 
+//   console.log(body.data.total)
   
-  // array of characters 
-  console.log(body.data.results)
-})
+//   // array of characters 
+//   console.log(body.data.results)
+// })
 
 // var cName = prompt("Input the name of the character you want, ");
 
-// marvel.characters.findByName(cName).then(function(res){
-// 	console.log(cName + "'s id is "+ res.data[0].id);
+// marvel.characters.findByName("spiderman").then(function(res){
+// 	console.log("Spiderman's id is "+ res.data[0]);
+//   return marvel.characters.comics(res.data[0].id);
+
+// })
+// .fail(function(){
+//   console.log('error');
 // })
 // .done();
 // //return 
+
+//this code finds a character based on the name given. Hopefully will be determined by user
+//input
+marvel.characters.findByName('spider-man',function(err, results) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(results.data[0].id);
+});
+
+//this block allows the server to render the html
+http.createServer(function (req, res) {
+  //fs reads the defined html file
+    fs.readFile ('index.html',function(err,data){
+      //header is required to tell server what kind of data is being read
+      res.writeHead(200, {'Content-Type': 'text/html  '});
+      //res.write('Hello World!');
+      //writes the html to the page with data from index.html
+      res.write(data);
+      res.end();
+    });
+}).listen(8080);
+
--- testscript.js
+++ testscript.js
@@ -1,38 +1,38 @@
-var url = 'http(s)://gateway.marvel.com:80/';
-var characters = 'v1/public/comics';
-// var cName =  prompt("Char name");
+// var url = 'http(s)://gateway.marvel.com:80/';
+// var characters = 'v1/public/comics';
+// // var cName =  prompt("Char name");
 
-var PRIVATE_KEY = '16daedc6fc0a02f239a9ad700e10b526498a9afd';
-var PUBLIC_KEY= 'b418924bd54847ace74ceaa0e9b44017';
+// var PRIVATE_KEY = '16daedc6fc0a02f239a9ad700e10b526498a9afd';
+// var PUBLIC_KEY= 'b418924bd54847ace74ceaa0e9b44017';
 
-function getData(){
- var ts = new Date().getTime();
- var hash = CryptoJS.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
- // var hash = md5(ts + PUBLIC_KEY + PRIVATE_KEY).toString();
-console.log(hash);
+// function getData(){
+//  var ts = new Date().getTime();
+//  var hash = CryptoJS.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
+//  // var hash = md5(ts + PUBLIC_KEY + PRIVATE_KEY).toString();
+// console.log(hash);
 
- var charID = '1009718'
+//  var charID = '1009718'
 
- // var c_url = url+characters;
- // var c_url = 'https://gateway.marvel.com:443/v1/public/characters?name=spiderman&ts='+ts+'&apikey='+PUBLIC_KEY+'&hash='+hash;
-var c_url = 'http://gateway.marvel.com/v1/public/comics?';
- console.log(c_url);
+//  // var c_url = url+characters;
+//  // var c_url = 'https://gateway.marvel.com:443/v1/public/characters?name=spiderman&ts='+ts+'&apikey='+PUBLIC_KEY+'&hash='+hash;
+// var c_url = 'http://gateway.marvel.com/v1/public/comics?';
+//  console.log(c_url);
 
- $.getJSON(c_url,{
- 	ts:ts,
- 	apikey: PUBLIC_KEY,
- 	hash: hash,
- 	characters: charID
- })
- .done(function(data){
- 	console.log(data);
- })
- .fail(function(err){
- 	console.log(err);
- })
-};
+//  $.getJSON(c_url,{
+//  	ts:ts,
+//  	apikey: PUBLIC_KEY,
+//  	hash: hash,
+//  	characters: charID
+//  })
+//  .done(function(data){
+//  	console.log(data);
+//  })
+//  .fail(function(err){
+//  	console.log(err);
+//  })
+// };
 
-getData();
+// getData();
 
 // $(function(){
 // var marvelAPI = 'https://gateway.marvel.com/v1/public/comics';
@@ -53,5 +53,9 @@ getData();
 //       output += '</ul>'
 //       $('#results').append(output);
 //   });
-   
 // });
+
+$(document).ready(
+	function(){
+		console.log("Ready");
+	});
